25ab8ba5415bd2fc25f7b807d67de3a29f4272c6
mare203 <begovic444@gmail.com>
2025-06-13 18:56:44 +0200

    Test Commit

---
+++ src/main/java/org/basketrolling/BasketRolling.java
@@ -0,0 +1,12 @@
+package org.basketrolling;
+
+/**
+ *
+ * @author Marko
+ */
+public class BasketRolling {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
