dff775d4212f17e84805768f54adae4fa9123f4d
mare203 <begovic444@gmail.com>
2025-06-14 22:04:52 +0200

    Beans bearbeitet. closes #2

--- src/main/java/org/basketrolling/beans/Login.java
+++ src/main/java/org/basketrolling/beans/Login.java
@@ -4,17 +4,39 @@
  */
 package org.basketrolling.beans;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.UUID;
+import org.basketrolling.enums.Rolle;
 
 /**
  *
  * @author Marko
  */
+@Entity
+@Table(name = "login")
 public class Login {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, updatable = false)
     private UUID loginId;
+
+    @Column(name = "benutzername", nullable = false, unique = true)
     private String benutzername;
+    
+    @Column(name = "passwort", nullable = false)
     private String passwort;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rolle", nullable = false)
+    private Rolle rolle;
 
     public UUID getLoginId() {
         return loginId;
@@ -39,10 +61,17 @@ public class Login {
     public void setPasswort(String passwort) {
         this.passwort = passwort;
     }
+        
+    public Rolle getRolle() {
+        return rolle;
+    }
+
+    public void setRolle(Rolle rolle) {
+        this.rolle = rolle;
+    }
 
     @Override
     public String toString() {
         return "Login{" + "loginId=" + loginId + ", benutzername=" + benutzername + ", passwort=" + passwort + '}';
     }
-   
 }
