0d3dde106dfa25c289f594d617d2f8cd82e3db63
mare203 <begovic444@gmail.com>
2025-07-19 19:49:28 +0200

    module-info aktualisiert

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -17,6 +17,8 @@ module BasketRolling {
     opens org.basketrolling to org.hibernate.orm.core, org.postgresql.jdbc;
     opens org.basketrolling.beans to org.hibernate.orm.core;
     opens org.basketrolling.gui.controller to javafx.fxml;
+    opens org.basketrolling.gui.controller.hinzufuegen to javafx.fxml;
+    opens org.basketrolling.gui.controller.bearbeiten to javafx.fxml;
     opens org.basketrolling.gui.menues to javafx.fxml, javafx.graphics;
     
      exports org.basketrolling;
@@ -24,6 +26,8 @@ module BasketRolling {
      exports org.basketrolling.dao;
      exports org.basketrolling.enums;
      exports org.basketrolling.gui.controller;
+     exports org.basketrolling.gui.controller.hinzufuegen;
+     exports org.basketrolling.gui.controller.bearbeiten;
      exports org.basketrolling.interfaces;
      exports org.basketrolling.service;
      exports org.basketrolling.utils;
